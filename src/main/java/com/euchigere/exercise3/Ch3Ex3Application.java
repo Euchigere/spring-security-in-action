d579b393522ae2c7bc93f1ef9bced5b92f9252be
euchigere <ugochigere02@gmail.com>
2021-03-10 18:56:14 +0100

    add exercise 3

---
+++ src/main/java/com/euchigere/exercise3/Ch3Ex3Application.java
@@ -0,0 +1,11 @@
+package com.euchigere.exercise3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3Ex3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(com.euchigere.exercise3.Ch3Ex3Application.class, args);
+    }
+}
