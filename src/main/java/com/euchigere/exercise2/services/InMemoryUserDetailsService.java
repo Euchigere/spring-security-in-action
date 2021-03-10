443f5f27423dce8260244f665fb25832bd34d48b
euchigere <ugochigere02@gmail.com>
2021-03-10 18:55:43 +0100

    add exercise 2

---
+++ src/main/java/com/euchigere/exercise2/services/InMemoryUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.euchigere.exercise2.services;
+
+import lombok.Data;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+@Data
+public class InMemoryUserDetailsService implements UserDetailsService {
+    private List<UserDetails> users;
+
+    public InMemoryUserDetailsService(List<UserDetails> users) {
+        this.users = users;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return users.stream()
+                .filter(
+                        u -> u.getUsername().equals(username)
+                )
+                .findFirst()
+                .orElseThrow(
+                        () -> new UsernameNotFoundException("User not found")
+                );
+    }
+}
