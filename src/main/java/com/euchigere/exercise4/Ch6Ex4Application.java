865656c93116e0cb4c8ebbb5203996c08578b742
euchigere <ugochigere02gmail.com>
2021-04-07 00:08:26 +0100

    add new application exercise

---
+++ src/main/java/com/euchigere/exercise4/Ch6Ex4Application.java
@@ -0,0 +1,11 @@
+package com.euchigere.exercise4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch6Ex4Application {
+    public static void main(String[] args) {
+        SpringApplication.run(com.euchigere.exercise4.Ch6Ex4Application.class, args);
+    }
+}
