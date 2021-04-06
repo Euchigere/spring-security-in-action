562c99a0552f4abeb30d35c1378864ca5eae1e4a
euchigere <ugochigere02gmail.com>
2021-04-07 00:13:41 +0100

    add data and schema for exercise 4

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,35 @@
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(45) NOT NULL,
-    `password` VARCHAR(45) NOT NULL,
-    `enabled` INT NOT NULL,
+-- Data base schema for excercise 3
+-- CREATE TABLE IF NOT EXISTS `users` (
+--     `id` INT NOT NULL AUTO_INCREMENT,
+--     `username` VARCHAR(45) NOT NULL,
+--     `password` VARCHAR(45) NOT NULL,
+--     `enabled` INT NOT NULL,
+--     PRIMARY KEY (`id`));
+--
+-- CREATE TABLE IF NOT EXISTS `authorities` (
+--     `id` INT NOT NULL AUTO_INCREMENT,
+--     `username` VARCHAR(45) NOT NULL,
+--     `authority` VARCHAR(45) NOT NULL,
+--     PRIMARY KEY (`id`));
+
+
+-- Data base data for exercise 4
+CREATE TABLE IF NOT EXISTS `user` (
+   `id` INT NOT NULL AUTO_INCREMENT,
+   `username` VARCHAR(45) NOT NULL,
+    `password` TEXT NOT NULL,
+    `algorithm` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`id`));
 
-CREATE TABLE IF NOT EXISTS `authorities` (
+CREATE TABLE IF NOT EXISTS `authority` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(45) NOT NULL,
-    `authority` VARCHAR(45) NOT NULL,
+    `name` VARCHAR(45) NOT NULL,
+    `user` INT NOT NULL,
+    PRIMARY KEY (`id`));
+
+CREATE TABLE IF NOT EXISTS `product` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(45) NOT NULL,
+    `price` VARCHAR(45) NOT NULL,
+    `currency` VARCHAR(45) NOT NULL,
     PRIMARY KEY (`id`));
