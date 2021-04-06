40d6e63f852aa3936a1563e398b2d5371b1a3edd
euchigere <ugochigere02gmail.com>
2021-04-07 00:12:51 +0100

    add SecurityConfig

---
+++ src/main/java/com/euchigere/exercise4/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.euchigere.exercise4.config;
+
+import com.euchigere.exercise4.services.AuthenticationProviderService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AuthenticationProviderService authenticationProvider;
+
+    public SecurityConfig(AuthenticationProviderService authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http)
+            throws Exception {
+        http.formLogin()
+                .defaultSuccessUrl("/main", true);
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
+}
