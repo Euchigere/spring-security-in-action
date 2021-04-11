00848d8f6d799b8fbb2e6eb55e04b77d462cb226
euchigere <ugochigere02gmail.com>
2021-04-11 13:57:16 +0100

    new excercise

---
+++ business-logic/src/main/java/com/euchigere/exercise6/Ch12Ex6Application.java
@@ -0,0 +1,11 @@
+package com.euchigere.exercise6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch12Ex6Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Ch12Ex6Application.class, args);
+    }
+}
---
+++ business-logic/src/main/java/com/euchigere/exercise6/controllers/MainController.java
@@ -0,0 +1,12 @@
+package com.euchigere.exercise6.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main() {
+        return "main.html";
+    }
+}
