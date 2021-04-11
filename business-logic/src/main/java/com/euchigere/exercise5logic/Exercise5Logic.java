486ec67ae56ebd615a92b759259ee544ea051b9b
euchigere <ugochigere02gmail.com>
2021-04-11 10:08:05 +0100

    add new module for business logic server

---
+++ business-logic/src/main/java/com/euchigere/exercise5logic/Exercise5Logic.java
@@ -0,0 +1,11 @@
+package com.euchigere.exercise5logic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise5Logic {
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise5Logic.class, args);
+    }
+}
---
+++ business-logic/src/test/java/com/euchigere/exercise5logic/Exercise5LogicTests.java
@@ -0,0 +1,13 @@
+package com.euchigere.exercise5logic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise5LogicTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
